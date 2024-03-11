ab56ff818ad3719dfc21ad5c71e26ea96502fc95
aleanrocha <alean.oficial.me@gmail.com>
2024-03-11 09:09:37 -0300

    chore: add global styles and fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import GlobalStyles from './styles/globalStyles'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyles />
     <h1>Hello World</h1>
   </React.StrictMode>
 )
