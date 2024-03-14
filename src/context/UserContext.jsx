91a271813829ee3b2e6f6f2ccbc22e3d35763d9a
aleanrocha <alean.oficial.me@gmail.com>
2024-03-14 14:45:01 -0300

    feat: save user information to localStorage

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,13 +1,25 @@
 import PropTypes from 'prop-types'
-import { createContext, useState } from 'react'
-
+import { createContext, useEffect, useState } from 'react'
 export const UserContext = createContext({})
 
 export const UserProvider = ({ children }) => {
   const [userData, setUserData] = useState({})
   const putUserData = (data) => {
-    setUserData(data)
+    setUserData((prevUserData) => {
+      const newData = { ...prevUserData, ...data }
+      return localStorage.setItem('clientData', JSON.stringify(newData))
+    })
+    console.log(userData)
   }
+  useEffect(() => {
+    const loadClientData = async () => {
+      const clientDataString = await localStorage.getItem('clientData')
+      if (clientDataString) {
+        setUserData(JSON.parse(clientDataString))
+      }
+    }
+    loadClientData()
+  }, [])
   return (
     <UserContext.Provider value={{ putUserData }}>
       {children}
