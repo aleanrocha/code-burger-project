f40c5f39f268c1b1b6d4b1e19e37d5e9874a73f2
aleanrocha <alean.oficial.me@gmail.com>
2024-03-22 16:30:08 -0300

    feat: add JWT token to API calls

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,11 @@ const apiCodeBurger = axios.create({
   baseURL: 'http://localhost:3000'
 })
 
+apiCodeBurger.interceptors.request.use(async (config) => {
+  const userDataString = localStorage.getItem('clientData')
+  const userData = userDataString && JSON.parse(userDataString).token
+  config.headers.Authorization = `Bearer ${userData}`
+  return config
+})
+
 export default apiCodeBurger
