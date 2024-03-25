b1bfe1800516a5058920490d6d25321660a0e62e
aleanrocha <alean.oficial.me@gmail.com>
2024-03-25 06:48:13 -0300

    feat: create and style entire category carousel component

--- src/components/CategoryCarousel/index.jsx
+++ src/components/CategoryCarousel/index.jsx
@@ -1,18 +1,78 @@
-import { useEffect } from 'react'
+import { useEffect, useState, useRef } from 'react'
+import { register } from 'swiper/element/bundle'
 
 import api from '../../services/api'
-import { CategoryContainer } from './styles'
+import Button from '../Button'
+import { CategoryContainer, CategoryTitle, SlideContent, Image } from './styles'
+
+register()
 
 const CategoryCarousel = () => {
+  const [categories, setCategories] = useState([])
+  const [slidePerView, setSlidePerView] = useState()
+  const swiperRef = useRef(null)
+
   useEffect(() => {
     const loadCategories = async () => {
-      const res = await api.get('/categories')
-      return res
+      const { data } = await api.get('/categories')
+      console.log(data)
+      setCategories(data)
     }
-    console.log(loadCategories())
+    loadCategories()
   }, [])
 
-  return <CategoryContainer></CategoryContainer>
+  useEffect(() => {
+    const swiperContainer = swiperRef.current
+    const params = {
+      pagination: true,
+      injectStyles: [
+        `
+          .swiper-pagination-bullet{
+            background-color: #9758a4;
+          }
+      `
+      ]
+    }
+    Object.assign(swiperContainer, params)
+    swiperContainer.initialize()
+
+    const handleResiize = () => {
+      if (window.innerWidth <= 580) {
+        setSlidePerView(1)
+      } else if (window.innerWidth <= 768) {
+        setSlidePerView(2)
+      } else if (window.innerWidth <= 992) {
+        setSlidePerView(3)
+      } else {
+        setSlidePerView(4)
+      }
+    }
+    handleResiize()
+    window.addEventListener('resize', handleResiize)
+    return () => window.removeEventListener('resize', handleResiize)
+  }, [])
+
+  return (
+    <CategoryContainer>
+      <CategoryTitle>Categorias</CategoryTitle>
+      <swiper-container
+        slides-per-view={slidePerView}
+        pagination="true"
+        space-between="10"
+        ref={swiperRef}
+        init="false"
+      >
+        {categories.map((category) => (
+          <swiper-slide key={category.id}>
+            <SlideContent>
+              <Image src={category.url} alt="comida deliciosa" />
+              <Button text={category.name} />
+            </SlideContent>
+          </swiper-slide>
+        ))}
+      </swiper-container>
+    </CategoryContainer>
+  )
 }
 
 export default CategoryCarousel
--- src/components/CategoryCarousel/styles.jsx
+++ src/components/CategoryCarousel/styles.jsx
@@ -2,4 +2,43 @@ import styled from 'styled-components'
 
 export const CategoryContainer = styled.section`
   width: 100%;
+  padding: 2rem;
+
+  > swiper-container {
+    width: 100%;
+    max-width: 1040px;
+    margin: auto;
+    cursor: grab;
+
+    @media screen and (max-width: 580px) {
+      width: 300px;
+    }
+  }
+
+  @media screen and (max-width: 580px) {
+    padding: 1rem;
+  }
+`
+
+export const CategoryTitle = styled.h2`
+  color: #9758a4;
+  text-align: center;
+  font-size: 2rem;
+  text-transform: uppercase;
+`
+
+export const SlideContent = styled.div`
+  width: 100%;
+  max-width: 300px;
+  background-color: aliceblue;
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+  margin: 2rem 0 3rem;
+`
+
+export const Image = styled.img`
+  width: 100%;
+  height: 199px;
+  border-radius: 0.5rem;
 `
