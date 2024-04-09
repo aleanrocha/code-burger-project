ad6350e1b20f313326c824579d243740184cfd6a
aleanrocha <alean.oficial.me@gmail.com>
2024-04-09 06:59:40 -0300

    feat: add application footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import { FooterContainer } from './styles'
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <p>
+        &copy; 2024 - Desenvolvido por
+        <a target="_blank" href="https://linkedin.com/in/aleanrocha">
+          {' '}
+          Alean Rocha
+        </a>
+      </p>
+    </FooterContainer>
+  )
+}
+
+export default Footer
---
+++ src/components/Footer/styles.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const FooterContainer = styled.footer`
+  width: 100%;
+  background-color: #28162c;
+  color: #fafafa;
+  padding: 2rem 1rem;
+  text-align: center;
+
+  a {
+    text-decoration: none;
+    color: #9758a6;
+  }
+`
