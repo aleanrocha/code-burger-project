df0ef5d084ad665d1954b709e8c8a2376433dc64
aleanrocha <alean.oficial.me@gmail.com>
2024-04-18 01:52:32 -0300

    feat: update base URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiCodeBurger = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://code-burger-api.up.railway.app/'
 })
 
 apiCodeBurger.interceptors.request.use(async (config) => {
